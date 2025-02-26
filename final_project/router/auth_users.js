bb7f94f241e5226c7e449972ef7bfd52d6d64534
adhilchalil <chaliladhil@gmail.com>
2025-02-26 04:28:12 -0500

    project changes added

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,22 +7,83 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    //write code to check if username and password match the one we have in records.
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: {username,password}
+            // IMPORTANT AF!!!! Very bad practice to include password in payload.
+            // In HMAC token creation it is only base64 encoded and can be decoded out.
+        }, 'token_secret', { expiresIn: 60 * 60 });
+
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    let isbn = req.params.isbn;
+    let review = req.body.review;
+    if(isbn){
+        if(books[isbn]){
+            let reviews = books[isbn].reviews;
+            let reviewExists = reviews[req.user]? true: false;
+            reviews[req.user] = review;
+            return res.send(reviewExists? "Review updated!": "Review added!");
+        }
+        return res.status(404).json({message: "Book not found!"});
+    }
+    return res.status(403).json({message: "No ID requested!"});
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    let isbn = req.params.isbn;
+    let review = req.body.review;
+    if(isbn){
+        if(books[isbn]){
+            let reviews = books[isbn].reviews;
+            let reviewExists = reviews[req.user]? true: false;
+            reviews[req.user] = review;
+            return res.send(reviewExists? "Review updated!": "Review added!");
+        }
+        return res.status(404).json({message: "Book not found!"});
+    }
+    return res.status(403).json({message: "No ID requested!"});
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,37 +7,117 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if both username and password are provided
+    if (username && password) {
+    // Check if the user does not already exist
+        if (!doesExist(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',async function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    new Promise((resolve, reject) => {
+        setTimeout(() => resolve(JSON.stringify(books, null, 4)), 2000);
+    }).then((allBooks) => {
+        res.send(allBooks);
+    }).catch((err) => {
+        res.status(404).json({message: "Something went wrong!"})
+    });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let isbn = Number(req.params.isbn);
+            if(isbn){
+                if(books[isbn]){
+                    resolve({status: 200, data: books[isbn]});
+                }
+                resolve({status: 403, data: "Book not found!"});
+            }
+            resolve({status: 404, data: "Invalid ID!"});
+        }, 2000);
+    }).then((resp) => {
+        res.status(resp.status).send(JSON.stringify(resp.data, null, 4));
+    }).catch((err) => {
+        res.status(404).json({message: "Something went wrong!"});
+    });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let author = req.params.author;
+            let bookList = {};
+            if(author){
+                for(let isbn in books){
+                    if(books[isbn].author == author){
+                        bookList[isbn] = books[isbn];
+                    }
+                };
+                resolve({status: 200, data: bookList});
+            }
+            resolve({status: 403, data: "No author requested!"});
+        }, 2000);
+    }).then((resp) => {
+        res.status(resp.status).send(JSON.stringify(resp.data, null, 4));
+    }).catch((err) => {
+        res.status(404).json({message: "Something went wrong!"})
+    });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let title = req.params.title;
+            let bookList = {};
+            if(title){
+                for(let isbn in books){
+                    if(books[isbn].title == title){
+                        bookList[isbn] = books[isbn];
+                    }
+                };
+                resolve({status: 200, data: bookList});
+            }
+            resolve({status: 403, data: "No title requested!"});
+        },2000);
+    }).then((resp) => {
+        res.status(resp.status).send(JSON.stringify(resp.data, null, 4));
+    }).catch((err) => {
+        res.status(404).json({message: "Something went wrong!"})
+    });
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    if(isbn){
+        if(books[isbn]){
+            let reviews = books[isbn].reviews;
+            return res.send(JSON.stringify(reviews, null, 4));
+        }
+        return res.status(404).json({message: "Book not found!"});
+    }
+    return res.status(403).json({message: "No ID requested!"});
 });
 
 module.exports.general = public_users;
